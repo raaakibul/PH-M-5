42f2596fce0fde6c96db2af11e73415554e15151
rakibul-islam <raakibul.uap@gmail.com>
2024-09-05 23:07:54 +0600

    document object model checked

--- Module-24/dom.js
+++ Module-24/dom.js
@@ -1,3 +1,11 @@
 console.log("dom");
 console.log(document);
 // document object model
+// document.getElementsByTagName("body");
+const student ={
+    name: "Student",
+    Id: 11,
+    study: function(time){
+        console.log("Study Time");
+    }
+}
