377d905dec577d0b18f626bd0b46036c1042239c
rakibul-islam <raakibul.uap@gmail.com>
2024-09-05 23:16:12 +0600

    h1 heading

--- Module-24/dom.js
+++ Module-24/dom.js
@@ -9,10 +9,17 @@ console.log(document);
 //         console.log("Study Time");
 //     }
 // }
-
+// li
 const liCollection = document.getElementsByTagName('li');
 // console.log(liCollection);
 
 for (const li of liCollection){
-    console.log(li);
+    console.log(li.innerText);
 }
+
+const allHeadings = document.getElementsByTagName('h1');
+for (const h1 of allHeadings){
+    console.log(h1.innerText);
+}
+
+
