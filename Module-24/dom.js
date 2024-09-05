2304927ad41d812679b0960137413046d78c936f
rakibul-islam <raakibul.uap@gmail.com>
2024-09-05 23:13:47 +0600

    li collection

--- Module-24/dom.js
+++ Module-24/dom.js
@@ -1,11 +1,18 @@
-console.log("dom");
+console.log("Dom");
 console.log(document);
 // document object model
 // document.getElementsByTagName("body");
-const student ={
-    name: "Student",
-    Id: 11,
-    study: function(time){
-        console.log("Study Time");
-    }
+// const student ={
+//     name: "Student",
+//     Id: 11,
+//     study: function(time){
+//         console.log("Study Time");
+//     }
+// }
+
+const liCollection = document.getElementsByTagName('li');
+// console.log(liCollection);
+
+for (const li of liCollection){
+    console.log(li);
 }
